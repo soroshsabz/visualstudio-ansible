196f9bb9cf55493b6b27fc20ed3bef2b5a6ff141
soroshsabz <soorosh_abi@hotmail.com>
2023-02-16 20:30:45 +0330

    Add Grammars. (see #5)

---
+++ Source/Ansible.VisualStudio/AnsibleContentTypeDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ansible.VisualStudio
+{
+#pragma warning disable 649
+    public class AnsibleContentDefinition
+    {
+        [Export]
+        [Name("ansible")]
+        [BaseDefinition(CodeRemoteContentDefinition.CodeRemoteContentTypeName)]
+        internal static ContentTypeDefinition KamailioContentTypeDefinition;
+
+        [Export]
+        [FileExtension(".ansible.yaml")]
+        [ContentType("ansible")]
+        internal static FileExtensionToContentTypeDefinition KamailioFileExtensionDefinition;
+    }
+#pragma warning restore 649
+}
