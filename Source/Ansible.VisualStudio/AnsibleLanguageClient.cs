dde4159eaf6f02168457e45bb5cfd9a8e55da5ea
soroshsabz <soorosh_abi@hotmail.com>
2023-02-17 19:17:15 +0330

    Update language client. (see #4)

--- Source/Ansible.VisualStudio/AnsibleLanguageClient.cs
+++ Source/Ansible.VisualStudio/AnsibleLanguageClient.cs
@@ -1,12 +1,51 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.LanguageServer.Client;
+using Microsoft.VisualStudio.Threading;
+using Microsoft.VisualStudio.Utilities;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ansible.VisualStudio
 {
-    public class AnsibleLanguageClient
+    [ContentType("ansible")]
+    [Export(typeof(ILanguageClient))]
+    public class AnsibleLanguageClient : ILanguageClient
     {
+        public string Name => "Ansible Language Extension";
+
+        public IEnumerable<string> ConfigurationSections => null;
+
+        public object InitializationOptions => null;
+
+        public IEnumerable<string> FilesToWatch => null;
+
+        public bool ShowNotificationOnInitializeFailed => true;
+
+        public event AsyncEventHandler<EventArgs> StartAsync;
+        public event AsyncEventHandler<EventArgs> StopAsync;
+
+        public Task<Connection> ActivateAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnLoadedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OnServerInitializedAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<InitializationFailureContext> OnServerInitializeFailedAsync(ILanguageClientInitializationInfo initializationState)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
